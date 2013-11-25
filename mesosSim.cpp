aa6c2ec70a632f1a482508d4791d4c9e925cb08e
frankli <frankli@frankli-ThinkPad-T520.(none)>
2013-11-25 01:18:40 -0800

    Fixed issue of deleting tasks from framework on task completion

--- mesosSim.cpp
+++ mesosSim.cpp
@@ -39,7 +39,6 @@ class Event{
 	}
 	public:
 		Event(){};
-		// TODO: Specify event types
 		enum EvtType{send_offer, accept_offer, reject_offer, finished_task};
 		Event(EvtType type, double etime, Msg msg):_type(type),_etime(etime), _msg(msg) {_id = global_event_id; global_event_id++;}
 		EvtType get_type(){return _type;}
@@ -66,6 +65,7 @@ typedef struct{
 	unsigned int task_id;
 	Resources used_resources;
 	double task_time;
+	bool being_run;
 } Task;
 
 class Slave{
@@ -97,6 +97,8 @@ int num_frameworks = 1;
 int max_slave_id = 0;
 unordered_map<unsigned int, int> map;
 
+double t_cpus = 0.0, t_mem = 0.0, t_disk = 0.0;
+
 // Functions handling events
 void init(Slave *n);	// ok
 void send_offer(Event e);
@@ -112,7 +114,6 @@ int main(int argc, char *argv[]){
 	allFrameworks = new Framework[num_frameworks];
 
 	// lets initialize slave state and the event queue 
-	double t_cpus = 0.0, t_mem = 0.0, t_disk = 0.0;
 	for(int i=0;i<num_slaves;i++){
 		init(&allSlaves[i]);
 		map[allSlaves[i].id]=i;
@@ -122,20 +123,11 @@ int main(int argc, char *argv[]){
 		t_disk += allSlaves[i].resources.disk;
 	}
 	
-	Resources master_rsrc = {t_cpus, t_mem, t_disk};
-
-        //cout << FutureEventList.empty() << endl;
 	while(!FutureEventList.empty()){
 		Event evt=FutureEventList.top();
 		FutureEventList.pop();
 		Clock=evt.get_time();
-		Slave * n;
-		n=&allSlaves[map[evt.get_id()]];
-		if(evt.get_id()!=n->id){
-			cout << evt.get_id() <<  " " << n->id  << endl;
-		}
 		
-		assert(evt.get_id()==n->id);
 		if(evt.get_type()==Event::send_offer){
 			send_offer(evt);
 		}else if(evt.get_type()==Event::accept_offer){
@@ -148,10 +140,6 @@ int main(int argc, char *argv[]){
 
 	}
 
-	while(!FutureEventList.empty()){
-		FutureEventList.pop();
-	}
-
 	return 0;
 }
 
@@ -162,13 +150,6 @@ void init(Slave *n){
 	n->resources = rsrc;
 	n->free_resources = rsrc;
 }
-
-/*bool free_slave(Slave s) {
-	if(s.cpus != 0 || s.mem != 0 || s.disk != 0) {
-		return true;
-	}
-	return false;
-}*/
 
 bool task_on_slave(Slave s, Resources r){
 	if(s.free_resources.cpus < r.cpus && s.free_resources.mem < r.mem && s.free_resources.disk < r.disk)
@@ -190,26 +171,31 @@ void release_resources(Slave* s, Resources r) {
 
 int curr_rr_offer = 0;
 vector<Task> curr_schedules;
+unsigned int num_offers = 0;
+bool make_offers = true;
 
 void send_offer(Event e){
-	Framework f = allFrameworks[curr_rr_offer];
-	for(int i = 0; i < f.task_lists.size(); i++) {
+	Framework* f = &allFrameworks[curr_rr_offer];
+	for(int i = 0; i < f->task_lists.size(); i++) {
+		if(f->task_lists[i].size() == 0 || f->task_lists[i][0].being_run)
+			continue;
 		for(int j = 0; j < num_slaves; j++) {
-			if(task_on_slave(allSlaves[j], f.task_lists[i][0].used_resources)){
-				Task todo_task = f.task_lists[i][0];
+			if(task_on_slave(allSlaves[j], f->task_lists[i][0].used_resources)){
+				Task todo_task = f->task_lists[i][0];
 				todo_task.slave_id = allSlaves[j].id;
 				curr_schedules.push_back(todo_task);
 				use_resources(&allSlaves[j], todo_task.used_resources);
-				f.task_lists[i].pop_front();
+				f->task_lists[i][0].being_run = true;
 			}
 		}
 	}
 	if(curr_schedules.size() > 0) {
-		for(int i = 0; i < curr_schedules.size(); i++) {
+		//Only needed if making offer to everyone, so everyone sees same view
+/*		for(int i = 0; i < curr_schedules.size(); i++) {
 			Task task = curr_schedules[i];
 			release_resources(&allSlaves[map[task.slave_id]], task.used_resources);
 
-		}
+		}*/
 		Event evt(Event::accept_offer, e.get_time(), NULL_MSG);
 		FutureEventList.push(evt);
 	}else {
@@ -222,24 +208,43 @@ void accept_offer(Event e){
 	for(int i = 0; i < curr_schedules.size(); i++) {
 		Task task = curr_schedules[i];
 		Slave * s = &allSlaves[map[task.slave_id]];
-		use_resources(s, task.used_resources);
+
+		//Only needed if making offer to everyone, so everyone sees same view
+		//use_resources(s, task.used_resources);
 		s->curr_tasks.push_back(task);
-		Event evt(Event::finished_task, e.get_time() + task.task_time, {task.slave_id, 0, task.task_id});
+		Event evt(Event::finished_task, e.get_time() + task.task_time, {task.slave_id, curr_rr_offer, task.task_id});
 		FutureEventList.push(evt);
 
 	}
 	curr_schedules.clear();
 	curr_rr_offer++;
+	num_offers++;
+	if(num_offers < num_frameworks){
+		Event evt(Event::send_offer, e.get_time(), NULL_MSG);
+		FutureEventList.push(evt);
+	}else {
+		num_offers = 0;
+	}
+
 }
 
 void reject_offer(Event e){
 	assert(curr_schedules.size()==0);
 	curr_rr_offer++;
+	num_offers++;
+	if(num_offers < num_frameworks){
+		Event evt(Event::send_offer, e.get_time(), NULL_MSG);
+		FutureEventList.push(evt);
+	}else {
+		num_offers = 0;
+	}
+
 }
 
 void finished_task(Event e){
 	unsigned int s_id = e.get_msg().to;
 	unsigned int t_id = e.get_msg().val;
+	unsigned int f_id = e.get_msg().from;
 	Slave * s = &allSlaves[map[s_id]];
 	int i = 0;
 	for(; i<s->curr_tasks.size(); i++){
@@ -249,4 +254,16 @@ void finished_task(Event e){
 		}
 	}
 	s->curr_tasks.erase(s->curr_tasks.begin() + i);
+	Framework* f = &allFrameworks[f_id];
+	for(int i = 0; i < f->task_lists.size(); i++) {
+		if(f->task_lists[i][0].task_id == t_id){
+			f->task_lists[i].pop_front();
+			break;
+		}
+	}
+
+
+	Event evt(Event::send_offer, e.get_time(), NULL_MSG);
+	FutureEventList.push(evt);
+
 }
